fde3b61dcc8f89f130b8deb4829f3327ba23ae85
Sergey A <sergalf81@mail.ru>
2023-10-02 11:14:59 +0300

    Добавлена программа к ДЗ 2

---
+++ C#_HW_2/Program.cs
@@ -0,0 +1,10 @@
+﻿Console.WriteLine("Input number: ");
+int i = Convert.ToInt32(Console.ReadLine());
+int sum = 0;
+while(i > 0)
+{
+    int num = i % 10;
+    i = i / 10;
+    sum = sum + num;
+}
+System.Console.WriteLine(sum);
