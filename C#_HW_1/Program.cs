a981c624b0476a71e89cc2e24670a2d96c0aef8f
Sergey A <sergalf81@mail.ru>
2023-10-02 10:56:43 +0300

    Написана программа для ДЗ 1

---
+++ C#_HW_1/Program.cs
@@ -0,0 +1,10 @@
+﻿System.Console.Write("Inpet number a: ");
+int a = Convert.ToInt32(Console.ReadLine());
+System.Console.Write("Input number b: ");
+int b = Convert.ToInt32(Console.ReadLine());
+int n = a;
+for(int i = 1; i < b; i++)
+{
+    n = n * a;
+}
+System.Console.WriteLine(n);
